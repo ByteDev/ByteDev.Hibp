e0d3d2443605cc7ce72ec08c5ca6db893f2b8306
bytedev <paulstanley@protonmail.com>
2019-07-22 18:54:11 +0800

    Added more unit tests

--- src/ByteDev.Hibp.UnitTests/HibpUriFactoryTests.cs
+++ src/ByteDev.Hibp.UnitTests/HibpUriFactoryTests.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Net;
-using System.Net.Mime;
 using ByteDev.Hibp.Request;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace ByteDev.Hibp.UnitTests
 {
@@ -57,6 +55,104 @@ namespace ByteDev.Hibp.UnitTests
             private static Uri Act(string emailAddress, HibpRequestOptions options = null)
             {
                 return HibpUriFactory.CreateBreachedAccountUri(emailAddress, options);
+            }
+        }
+
+        [TestFixture]
+        public class CreateBreachedSiteUri : HibpUriFactoryTests
+        {
+            private const string ServicePath = "breaches";
+
+            [Test]
+            public void WhenDomainIsNull_ThenReturnUriWithoutDomainParam()
+            {
+                var result = HibpUriFactory.CreateBreachedSiteUri(null);
+
+                Assert.That(result.ToString(), Is.EqualTo(ExpectedUrl()));
+            }
+
+            [Test]
+            public void WhenDomainIsSpecified_ThenReturnUriWithDomain()
+            {
+                var result = HibpUriFactory.CreateBreachedSiteUri("yahoo.com");
+
+                Assert.That(result.ToString(), Is.EqualTo(ExpectedUrl() + "?domain=yahoo.com"));
+            }
+
+            private string ExpectedUrl()
+            {
+                return $"{UrlBase}{ServicePath}";
+            }
+        }
+
+        [TestFixture]
+        public class CreateBreachSiteByNameUri : HibpUriFactoryTests
+        {
+            private const string ServicePath = "breach";
+
+            [Test]
+            public void WhenBreachNameIsNull_ThenThrowException()
+            {
+                Assert.Throws<ArgumentException>(() => HibpUriFactory.CreateBreachSiteByNameUri(null));
+            }
+
+            [Test]
+            public void WhenBreachNameIsEmpty_ThenThrowException()
+            {
+                Assert.Throws<ArgumentException>(() => HibpUriFactory.CreateBreachSiteByNameUri(string.Empty));
+            }
+
+            [Test]
+            public void WhenBreachNameSupplied_ThenReturnUriWithBreachName()
+            {
+                var result = HibpUriFactory.CreateBreachSiteByNameUri("000webhost");
+
+                Assert.That(result.ToString(), Is.EqualTo(ExpectedUrl() + "/000webhost"));
+            }
+
+            private string ExpectedUrl()
+            {
+                return $"{UrlBase}{ServicePath}";
+            }
+        }
+
+        [TestFixture]
+        public class CreateDataClassesUri : HibpUriFactoryTests
+        {
+            private const string ServicePath = "dataclasses";
+
+            [Test]
+            public void WhenCalled_ThenReturnsUri()
+            {
+                var result = HibpUriFactory.CreateDataClassesUri();
+
+                Assert.That(result.ToString(), Is.EqualTo($"{UrlBase}{ServicePath}"));
+            }
+        }
+
+        [TestFixture]
+        public class CreateAccountPastesUri : HibpUriFactoryTests
+        {
+            private const string ServicePath = "pasteaccount/";
+
+            [Test]
+            public void WhenEmailAddressIsNull_ThenThrowException()
+            {
+                Assert.Throws<ArgumentException>(() => HibpUriFactory.CreateAccountPastesUri(null));
+            }
+
+            [Test]
+            public void WhenEmailAddressIsEmpty_ThenThrowException()
+            {
+                Assert.Throws<ArgumentException>(() => HibpUriFactory.CreateAccountPastesUri(string.Empty));
+            }
+
+            [Test]
+            public void WhenEmailAddressSupplied_ThenReturnUriWithEmailAddress()
+            {
+                var result = HibpUriFactory.CreateAccountPastesUri(EmailAddress);
+
+                Assert.That(result.ToString(), Is.EqualTo($"{UrlBase}{ServicePath}{_emailAddressUrlEncoded}"));
             }
         }
     }
