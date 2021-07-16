4405d3de2d489f671e5f1df7cdb427e91f2e3224
Paul Stanley <paulstanley@protonmail.com>
2021-07-16 10:48:35 +0800

    Added more unit tests

--- tests/ByteDev.Hibp.UnitTests/HibpClientTests.cs
+++ tests/ByteDev.Hibp.UnitTests/HibpClientTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace ByteDev.Hibp.UnitTests
@@ -20,6 +22,8 @@ namespace ByteDev.Hibp.UnitTests
         [TestFixture]
         public class GetAccountBreachesAsync : HibpClientTests
         {
+            private const string EmailAddress = "john@somewhere.com";
+
             [Test]
             public void WhenRateLimitExceeded_AndRetryDisabled_ThenThrowException()
             {
@@ -29,8 +33,23 @@ namespace ByteDev.Hibp.UnitTests
                 };
 
                 var sut = new HibpClient(new HttpClient(new Return429RateLimitExceededResponseHandler()), options);
+                
+                Assert.ThrowsAsync<HibpClientException>(() => _ = sut.GetAccountBreachesAsync(EmailAddress));
+            }
 
-                Assert.ThrowsAsync<HibpClientException>(() => _ = sut.GetAccountBreachesAsync("john@somewhere.com"));
+            [Test]
+            public async Task WhenRateLimitExceededOnlyOnFirstRequest_AndRetryEnabled_ThenReturnOkResponse()
+            {
+                var options = new HibpClientOptions
+                {
+                    RetryOnRateLimitExceeded = true
+                };
+
+                var sut = new HibpClient(new HttpClient(new Return429RateLimitExceededResponseHandler()), options);
+
+                var result = await sut.GetAccountBreachesAsync(EmailAddress);
+
+                Assert.That(result.Count(), Is.EqualTo(3));
             }
         }
     }
--- tests/ByteDev.Hibp.UnitTests/Return429RateLimitExceededResponseHandler.cs
+++ tests/ByteDev.Hibp.UnitTests/Return429RateLimitExceededResponseHandler.cs
@@ -7,14 +7,33 @@ namespace ByteDev.Hibp.UnitTests
 {
     public class Return429RateLimitExceededResponseHandler : HttpMessageHandler
     {
-        private const string RateLimitExceededResponseBody = "{ \"statusCode\": 429, \"message\": \"Rate limit is exceeded. Try again in 5 seconds.\" }";
+        private const string OkResponseBody = "[{\"Name\": \"000webhost\"},{\"Name\": \"123RF\"},{\"Name\": \"2844Breaches\"}]";
+
+        private const string RateLimitExceededResponseBody = "{ \"statusCode\": 429, \"message\": \"Rate limit is exceeded. Try again in 2 seconds.\" }";
+
+        private int _requestCounter;
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var response = new HttpResponseMessage((HttpStatusCode) 429)
+            HttpResponseMessage response;
+
+            // Returns 429 on first request and 200 on the second request
+            if (_requestCounter < 1)
             {
-                Content = new JsonContent(RateLimitExceededResponseBody)
-            };
+                response = new HttpResponseMessage((HttpStatusCode) 429)
+                {
+                    Content = new JsonContent(RateLimitExceededResponseBody)
+                };
+            }
+            else
+            {
+                response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new JsonContent(OkResponseBody)
+                };
+            }
+
+            _requestCounter++;
             
             return Task.FromResult(response);
         }
